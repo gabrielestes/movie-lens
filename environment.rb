53f578e8723b9a577febe8fdccc38d9e351f8fe3
Gabriel Estes <gabriel.estes@gmail.com>
2016-10-07 22:38:15 -0400

    Add Active Rec conn

---
+++ environment.rb
@@ -0,0 +1,10 @@
+require 'active_record'
+require 'pg'
+
+ActiveRecord::Base.establish_connection(
+  adapter:  'postgresql', # or 'mysql2' or 'sqlite3'
+  host:     'localhost',
+  database: 'movie_lens',
+  username: 'USER',
+  password: 'PASSWORD'
+)
