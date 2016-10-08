84be0a82b65c3bd647d137be718820d0d0b67b73
Gabriel Estes <gabriel.estes@gmail.com>
2016-10-07 23:25:06 -0400

    Complete seed tables

---
+++ seeds.rb
@@ -0,0 +1,86 @@
+require 'CSV'
+require_relative 'environment'
+require_relative 'models/tables'
+
+class RatingsData
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def load_from_file
+    CSV.foreach(@filename, col_sep: "\t") do |line|
+      user_id = line[0]
+      movie_id = line[1]
+      score = line[2]
+
+    Rating.create(
+      user_id: user_id,
+      movie_id: movie_id,
+      score: score
+    )
+    end
+  end
+end
+
+class MovieData
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def load_from_file
+    CSV.foreach(@filename, encoding: 'iso-8859-1', col_sep: '|') do |line|
+      movie_id = line[0].to_i
+      title = line[1]
+      release_date = line[2]
+      url = line[3]
+
+      Movie.create(
+        movie_id: movie_id,
+        title: title,
+        release_date: release_date,
+        url: url
+      )
+    end
+  end
+end
+
+class UserData
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def load_from_file
+    CSV.foreach(@filename, encoding: 'iso-8859-1', col_sep: ',') do |line|
+      user_id = line[0].to_i
+      age = line[1].to_i
+      gender = line[2]
+      occupation = line[3]
+      zipcode = line[4].to_i
+
+      User.create(
+        user_id: user_id,
+        age: age,
+        gender: gender,
+        occupation: occupation,
+        zipcode: zipcode
+      )
+    end
+  end
+end
+
+def main
+  ratings_data = RatingsData.new('db/u.data')
+
+  ratings_data.load_from_file
+
+  movie_data = MovieData.new('db/u.item') # , ratings_data
+
+  movie_data.load_from_file
+
+  user_data = UserData.new('db/u.user')
+
+  user_data.load_from_file
+
+end
+
+main if __FILE__ == $PROGRAM_NAME
