5ca837e7e6226535b8e81cefd1ee254881220db2
Gabriel Estes <gabriel.estes@gmail.com>
2016-10-08 22:46:00 -0400

    Write method for avg rating

--- models/movie.rb
+++ models/movie.rb
@@ -1,9 +1,12 @@
 require 'active_record'
 require_relative '../db/migrate/002_create_movies.rb'
 
-
 class Movie < ActiveRecord::Base
   validates :title, presence: true
   has_many :ratings
   has_many :users, through: :ratings
+
+  def get_average_rating
+    self.ratings.average(:score).to_f.round(1)
+  end
 end
